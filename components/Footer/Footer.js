aca1d3651e99b36a32620b899f11408d4138b710
Phanio <phaniophrero@yahoo.com>
2023-07-26 22:05:06 +0200

    updated footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,23 +1,74 @@
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaTiktok,
+  FaYoutube,
+} from "react-icons/fa";
+
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
     <footer className="footer">
       <div className="footer-row2">
+        <ul className="footer-social-media-list">
+          <li className="footer-social-media-item">
+            {/* Facebook */}
+            <a href="#" className="footer-social-media-link">
+              <FaFacebookF />
+            </a>
+          </li>
+          <li className="footer-social-media-item">
+            {/* Instagram */}
+            <a href="#" className="footer-social-media-link">
+              <FaInstagram />
+            </a>
+          </li>
+          <li className="footer-social-media-item">
+            {/* TikTok */}
+            <a href="#" className="footer-social-media-link">
+              <FaTiktok />
+            </a>
+          </li>
+          <li className="footer-social-media-item">
+            {/* Twitter */}
+            <a href="#" className="footer-social-media-link">
+              <FaTwitter />
+            </a>
+          </li>
+          <li className="footer-social-media-item">
+            {/* YouTube */}
+            <a href="#" className="footer-social-media-link">
+              <FaYoutube />
+            </a>
+          </li>
+        </ul>
         <ul className="footer-row2-list">
           <li className="footer-row2-link">
-            <a href="#"> GDPR</a>
+            <a href="#">Contact Us</a>
           </li>
-          <span className="footer-row2-link-dot"></span>
           <li className="footer-row2-link">
-            <a href="#">TERMENI SI CONDITII</a>
+            <a href="#">FAQ</a>
           </li>
-          <span className="footer-row2-link-dot"></span>
           <li className="footer-row2-link">
-            <a href="#">POLITICA COOKIES</a>
+            <a href="#">GDPR</a>
+          </li>
+          <li className="footer-row2-link">
+            <a href="#">Privacy Policy</a>
+          </li>
+
+          {/* <span className="footer-row2-link-dot"></span> */}
+          <li className="footer-row2-link">
+            <a href="#">Terms & Conditions</a>
+          </li>
+          {/* <span className="footer-row2-link-dot"></span> */}
+          <li className="footer-row2-link">
+            <a href="#">Disclaimer</a>
           </li>
         </ul>
       </div>
+      <hr className="footer-horizonta-line" />
       <div className="footer-row3">
         <h3 className="footer-row3-year">{year} - Stock Force One</h3>
       </div>
